21657b07dbcd125d46b858a219c9e58655ee35a9
AyoubGhoula <ayoubghoula40@gmail.com>
2024-02-04 17:11:45 +0100

    739. Daily Temperatures

--- Medium/739. Daily Temperatures/739. Daily Temperatures.py
+++ Medium/739. Daily Temperatures/739. Daily Temperatures.py
@@ -1,11 +1,11 @@
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
         answer=[0]*len(temperatures)
         stack=[]
         for day,temp in enumerate(temperatures):
             while stack and temperatures[stack[-1]]<temp:
                 last_day=stack.pop()
-                t[last_day]=day-last_day
+                answer[last_day]=day-last_day
             stack.append(day)
         return answer
                     
